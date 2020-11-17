e6587462fc1f5af6674e7c151c38ce2e73e31cbe
Ngure Njoroge <ngurenjoroge@ngures-MacBook-Air.local>
2020-11-17 10:08:56 -0700

    created console logging for users of the site

---
+++ js/app.js
@@ -0,0 +1,32 @@
+// var Age = 18
+// var is_subscribed = confirm("Do you have a subscription")
+
+// if (age <= 18 && opinion == true) {
+//     console.log(Age + "is over 18");
+// }
+
+// // if (age < 18 && useris_subscribed == false) { console.log("are you over the age of 18") }
+
+// else if ()
+
+// else if ()
+
+if (age >= 18 && is_subscribed) {
+    console.log("User is 18 or older and subscribed!");
+} else if (Age < 18 && is_subscribed) {
+    console.log("user is under 18 and is_subscribed");
+} else if (Age >= 18 && !is_subscribed) {
+    console.log("User is 18 or older and is NOT subscribed");
+} else if (Age < 18 && !is_subscribed) {
+    console.log("user is under 18 and is NOT subscribed");
+} else {
+    console.log ("Something Went Wrong!");
+}
+
+
+
+
+
+
+
+
