3c6f0e69e65e6d8397143b7a8e56b755d776b466
Ngure Njoroge <ngurenjoroge@ngures-MacBook-Air.local>
2020-11-18 12:49:32 -0700

    added the 9b assigment to 9a

--- js/app.js
+++ js/app.js
@@ -11,16 +11,104 @@
 
 // else if ()
 
+var age = 12;
+var is_subscribed = false;
+
 if (age >= 18 && is_subscribed) {
     console.log("User is 18 or older and subscribed!");
-} else if (Age < 18 && is_subscribed) {
+} else if (age < 18 && is_subscribed) {
     console.log("user is under 18 and is_subscribed");
-} else if (Age >= 18 && !is_subscribed) {
-    console.log("User is 18 or older and is NOT subscribed");
-} else if (Age < 18 && !is_subscribed) {
-    console.log("user is under 18 and is NOT subscribed");
+} else if (age >= 18 && !is_subscribed) {
+    console.log("User is 18 or older and is NOT_subscribed");
+} else if (age < 18 && !is_subscribed) {
+    console.log("user is under 18 and is NOT_subscribed");
 } else {
     console.log ("Something Went Wrong!");
+}
+
+
+// week 9 b array assignment below------------------------------
+
+var userAges = [18, 90, 15, 28, 25]
+var subscriptionStatus = [true, false, false, true];
+
+
+
+
+for (var index = 0; index < 5; index++) {
+    var userAge = userAges[index];
+    var is_subscribed = subscriptionStatus[index];
+
+
+    if (userAge >= 18 && is_subscribed) {
+        console.log("User is 18 or older and subscribed!");
+    } else if (userAge < 18 && is_subscribed) {
+        console.log("user is under 18 and is_subscribed");
+    } else if (userAge >= 18 && !is_subscribed) {
+        console.log("User is 18 or older and is NOT subscribed");
+    } else if (userAge < 18 && !is_subscribed) {
+        console.log("user is under 18 and is NOT subscribed");
+    } else {
+        console.log("Something Went Wrong!");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for (var index = 0; index < userAges.length; index++) {
+    var userAge = userAges[index];
+    var is_subscribed = subscriptionStatus[index];
+
+
+    if (userAge >= 18 && is_subscribed) {
+        console.log("User is 18 or older and subscribed!");
+    } else if (userAge < 18 && is_subscribed) {
+        console.log("user is under 18 and is_subscribed");
+    } else if (userAge >= 18 && !is_subscribed) {
+        console.log("User is 18 or older and is NOT subscribed");
+    } else if (userAge < 18 && !is_subscribed) {
+        console.log("user is under 18 and is NOT subscribed");
+    } else {
+        console.log("Something Went Wrong!");
+    }
+
+}
+
+
+
+var counter = 0;
+while (counter < userAges.length) {
+    var userAge = userAges[counter];
+    var is_subscribed = subscriptionStatus[counter];
+
+
+    if (userAge >= 18 && is_subscribed) {
+        console.log("User is 18 or older and subscribed!");
+    } else if (userAge < 18 && is_subscribed) {
+        console.log("user is under 18 and is_subscribed");
+    } else if (userAge >= 18 && !is_subscribed) {
+        console.log("User is 18 or older and is NOT subscribed");
+    } else if (userAge < 18 && !is_subscribed) {
+        console.log("user is under 18 and is NOT subscribed");
+    } else {
+        console.log("Something Went Wrong!");
+    }
+
+    counter++;
+
 }
 
 
